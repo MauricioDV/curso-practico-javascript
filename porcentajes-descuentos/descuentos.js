eafd4b0e173fd8517813abd5c6778360b3fb64c6
MauricioDV <mdv89ned@gmail.com>
2022-06-02 17:50:27 -0400

    Concluido seccion de descuento por cupón

--- porcentajes-descuentos/descuentos.js
+++ porcentajes-descuentos/descuentos.js
@@ -20,14 +20,49 @@ function calcularPrecioConDescuento(precio, descuento){
 }
 
 function onClickButtonPriceDiscount(){
-    const inputPrice = document.getElementById("InputPrice");
-    const priceValue = Number(inputPrice.value);
+     const inputPrice = document.getElementById("InputPrice");
+     const priceValue = Number(inputPrice.value);
     
-    const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = Number(inputDiscount.value);
+   const inputDiscount = document.getElementById("InputDiscount");
+   const discountValue = Number(inputDiscount.value);
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+     const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
 
-    const resultP = document.getElementById("ResultP");
-    resultP.innerText = "El precio con descuento es: $" + precioConDescuento;
+     const resultP = document.getElementById("ResultP");
+     resultP.innerText = "El precio con descuento es: $" + precioConDescuento;
+ }
+
+//Descuentos por cupones
+// Implementtar cupon que tenga un descuento para ciertos productos
+
+function onClickButtonPriceDiscountDos(){
+    const cupones = ["plata","gold","platino"];
+
+    const inputPrice = document.getElementById("inputPrecio");
+    const priceValue = inputPrice.value;
+    
+    const inputCupon = document.getElementById("inputCupon");
+    const couponValue = inputCupon.value;
+
+    let descuento;
+
+     switch(couponValue){
+         case cupones[0]: 
+             descuento = 15;
+             break;
+         case cupones[1]:
+             descuento = 25;
+             break;
+         case cupones[2]:
+             descuento = 50;
+             break;
+         default:
+             descuento = 0;
+             break;
+     }
+    
+     const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+    
+     const resultPdos = document.getElementById("resultCupon");
+     resultPdos.innerText = "El precio con descuento son: $" + precioConDescuento;   
 }
